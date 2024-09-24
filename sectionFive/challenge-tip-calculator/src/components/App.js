82564f66d647095ac894fb4b038474b9b1478ccf
Eric Rhinebolt <erhinebo@gmail.com>
2024-09-24 00:10:10 -0400

    continue challenge-tip-calculator

--- sectionFive/challenge-tip-calculator/src/components/App.js
+++ sectionFive/challenge-tip-calculator/src/components/App.js
@@ -1,15 +1,28 @@
 import { useState } from "react";
 
+const tips = [
+  { key: "0", value: "Dissatisfied (0%)" },
+  { key: "1", value: "It was okay (5%)" },
+  { key: "2", value: "It was good (10%)" },
+  { key: "3", value: "Absolutely amazing! (20%)" },
+];
+
 export default function App() {
   const [total, setTotal] = useState("");
+  const [mainTip, setMainTip] = useState(tips[0].value);
+  const [friendTip, setFriendTip] = useState(tips[0].value);
   return (
     <div>
       <Bill total={total} onTotal={setTotal}>
         How much was the bill?
       </Bill>
-      <PercentTip />
-      <PercentTip />
-      <Total />
+      <PercentTip tip={mainTip} onTip={setMainTip}>
+        How did you like the service?
+      </PercentTip>
+      <PercentTip tip={friendTip} onTip={setFriendTip}>
+        How did your friend like the service?
+      </PercentTip>
+      <Total></Total>
       <Reset />
     </div>
   );
@@ -33,7 +46,23 @@ function Bill({ total, onTotal, children }) {
   );
 }
 
-function PercentTip() {}
+function PercentTip({ onTip, children }) {
+  function handleTip(e) {
+    console.log(e);
+    onTip(e.target.value);
+  }
+
+  return (
+    <div>
+      <span>{children}</span>
+      <select onChange={(el) => handleTip(el)}>
+        {tips.map((tip, i) => (
+          <option value={tip.value}>{tip.value}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
 
 function Total() {}
 
